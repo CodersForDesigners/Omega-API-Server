5970b2f8bed548ff943a3760f4d2f814302e7841
adi <adityabhat@lazaro.in>
2019-04-24 09:52:54 +0530

    Customers : Get by Phone number : Add an endpoint for this

---
+++ customers/get/by-phone-number/index.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ *
+ * This script fetches a user based on a phone number from the system
+ *
+ */
+
+ini_set( 'display_errors', 0 );
+ini_set( 'error_reporting', E_ALL );
+
+date_default_timezone_set( 'Asia/Kolkata' );
+
+// Do not let this script timeout
+set_time_limit( 0 );
+
+// Continue processing this script even if the user closes the tab, or
+//  	hits the ESC key
+ignore_user_abort( true );
+// Allow this script to triggered from another origin
+header( 'Access-Control-Allow-Origin: *' );
+// Remove / modify certain headers of the response
+header_remove( 'X-Powered-By' );
+header( 'Content-Type: application/json' );	// JSON format
+
+$input = &$_GET;
+
+
+
+
+
+/*
+ *
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+ * SCRIPT DEPENDENCIES
+ * /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *
+ */
+require __DIR__ . '/lib/crm.php';
+
+
+
+
+
+/*
+ *
+ * Preliminary input sanitization
+ *
+ */
+foreach ( $input as $key => &$value ) {
+	$value = trim( $value );
+}
+
+/*
+ *
+ * Check if the required inputs are **present**
+ *
+ */
+if ( empty( $input[ 'phoneNumber' ] ) ) {
+	$response[ 'statusCode' ] = 4001;
+	$response[ 'message' ] = 'Please provide a phone number.';
+	http_response_code( 400 );
+	die( json_encode( $response ) );
+}
+if ( empty( $input[ 'project' ] ) ) {
+	$response[ 'statusCode' ] = 4002;
+	$response[ 'message' ] = 'Please provide a project name.';
+	http_response_code( 400 );
+	die( json_encode( $response ) );
+}
+
+/*
+ *
+ * Extract the input fields and Check if they are **valid**
+ *
+ */
+// Phone number(s)
+if ( ! preg_match( '/^\+\d+$/', $input[ 'phoneNumber' ] ) ) {
+	$response[ 'statusCode' ] = 4003;
+	$response[ 'message' ] = 'Please provide a valid primary phone number.';
+	http_response_code( 400 );
+	die( json_encode( $response ) );
+}
+
+$phoneNumber = $input[ 'phoneNumber' ];
+$project = $input[ 'project' ];
+$client = explode( ' ', $project )[ 0 ];
+
+/*
+ *
+ * Fetch the customer record from the CRM
+ *
+ */
+try {
+
+	$customer = CRM\getCustomerByPhoneNumber( $phoneNumber, $client );
+
+	// If no customer was found
+	if ( empty( $customer ) ) {
+		$response[ 'statusCode' ] = 1;
+		$response[ 'message' ] = 'No matching customer was found.';
+		http_response_code( 404 );
+		die( json_encode( $response ) );
+	}
+
+	$response[ 'statusCode' ] = 0;
+	// $response[ 'data' ] = $customer;
+	$response[ 'data' ] = [
+		'recordType' => $customer[ 'recordType' ],
+		'_id' => $customer[ 'id' ] ?? '',
+		'uid' => $customer[ 'UID' ] ?? '',
+		'owner' => $customer[ 'Owner' ][ 'name' ],
+		'isProspect' => $customer[ 'isProspect' ] ?? false,
+		'project' => $project,
+		'name' => $customer[ 'Full_Name' ] ?? '',
+		'firstName' => $customer[ 'First_Name' ] ?? '',
+		'lastName' => $customer[ 'Last_Name' ] ?? '',
+		'phoneNumber' => $phoneNumber,
+		'email' => $customer[ 'Email' ] ?? '',
+		'_ Special Discount' => $customer[ 'Special_Discount' ] ?? null,
+		'_ Discount Valid Till' => $customer[ 'Discount_Valid_Till' ] ?? null
+	];
+
+} catch ( \Exception $e ) {
+
+	// Respond with an error
+	if ( $e->getCode() > 5000 )
+		$response[ 'statusCode' ] = $e->getCode();
+	else
+		$response[ 'statusCode' ] = -1;
+
+	$response[ 'message' ] = $e->getMessage();
+	http_response_code( 500 );
+
+}
+// Finally, respond back to the client
+die( json_encode( $response ) );
---
+++ customers/get/by-phone-number/lib/crm.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace CRM;
+
+ini_set( "display_errors", 0 );
+ini_set( "error_reporting", E_ALL );
+
+// Set the timezone
+date_default_timezone_set( 'Asia/Kolkata' );
+// Do not let this script timeout
+set_time_limit( 0 );
+
+
+
+
+/*
+ *
+ * Set constant values
+ *
+ */
+class DATA {
+	public static $apiUrl = 'https://www.zohoapis.com/crm/v2/';
+	// public static $apiUrl = 'https://sandbox.zohoapis.com/crm/v2/';
+	public static $authCredentials;
+	public static $operatorRelationMap = [
+		'=' => 'equals',
+		'^=' => 'starts_with'
+	];
+}
+
+/*
+ *
+ * Get the auth credentials
+ *
+ */
+$authCredentialsFilename = __DIR__ . '/../../../../__environment/configuration/zoho.json';
+if ( empty( realpath( $authCredentialsFilename ) ) )
+	sleep( 1 );
+DATA::$authCredentials = json_decode( file_get_contents( $authCredentialsFilename ), true );
+
+
+
+
+
+/*
+ * -----
+ * A generic API request function
+ * -----
+ */
+function getAPIResponse ( $endpoint, $method = 'GET', $data = [ ] ) {
+
+	$accessToken = DATA::$authCredentials[ 'access_token' ];
+
+	$httpRequest = curl_init();
+	curl_setopt( $httpRequest, CURLOPT_URL, $endpoint );
+	curl_setopt( $httpRequest, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $httpRequest, CURLOPT_USERAGENT, 'Zo Ho Ho' );
+	$headers = [
+		'Authorization: Zoho-oauthtoken ' . $accessToken,
+		'Cache-Control: no-cache, no-store, must-revalidate'
+	];
+	if ( ! empty( $data ) ) {
+		$headers[ ] = 'Content-Type: application/json';
+		curl_setopt( $httpRequest, CURLOPT_POSTFIELDS, json_encode( $data ) );
+	}
+	curl_setopt( $httpRequest, CURLOPT_HTTPHEADER, $headers );
+	curl_setopt( $httpRequest, CURLOPT_CUSTOMREQUEST, $method );
+	$response = curl_exec( $httpRequest );
+	curl_close( $httpRequest );
+
+	// die( $response );
+	$body = json_decode( $response, true );
+
+	if ( empty( $body ) )
+		return [ ];
+		// throw new \Exception( 'Response is empty.', 10 );
+
+	// If an error occurred
+	if ( ! empty( $body[ 'code' ] ) ) {
+		if ( $body[ 'code' ] == 'INVALID_TOKEN' )
+			throw new \Exception( 'Access token is invalid.', 5001 );
+		if ( $body[ 'code' ] == 'AUTHENTICATION_FAILURE' )
+			throw new \Exception( 'Failure in authenticating.', 5002 );
+	}
+
+	return $body;
+
+}
+
+
+
+/*
+ * -----
+ * Criterion stringifier
+ * -----
+ */
+function getStringifiedCriterion ( $name, $relation__value ) {
+
+	if ( empty( $relation__value ) ) {
+		$criteriaString = '';
+	}
+	else if ( is_array( $relation__value ) ) {
+		$operator = $relation__value[ 0 ];
+		$value = $relation__value[ 1 ];
+		$criteriaString = '(' . $name . ':' . DATA::$operatorRelationMap[ $operator ] . ':' . urlencode( $value ) . ')';
+	}
+	else {
+		$value = $relation__value;
+		$criteriaString = '(' . $name . ':equals:' . urlencode( $value ) . ')';
+	}
+
+	return $criteriaString;
+
+}
+/*
+ * -----
+ * Criteria resolver
+ * -----
+ */
+function getResolvedCriteria ( $criteria ) {
+
+	$name = array_keys( $criteria )[ 0 ];
+
+	if ( in_array( $name, [ 'or', 'and' ] ) ) {
+		$operator = $name;
+		$subCriteria = $criteria[ $operator ];
+		$subCriteriaStrings = array_map( function ( $name, $value ) {
+			return getResolvedCriteria( [ $name => $value ] );
+		}, array_keys( $subCriteria ), array_values( $subCriteria ) );
+		return '(' . implode( $operator, $subCriteriaStrings ) . ')';
+	}
+	else {
+		return getStringifiedCriterion(
+			array_keys( $criteria )[ 0 ],
+			array_values( $criteria )[ 0 ]
+		);
+	}
+
+}
+
+
+
+function getRecordWhere ( $recordType, $criteria = [ ] ) {
+
+	$baseURL = DATA::$apiUrl . $recordType . '/search';
+	$criteriaString = '?criteria=(' . getResolvedCriteria( $criteria ) . ')';
+	$endpoint = $baseURL . $criteriaString;
+
+	$responseBody = getAPIResponse( $endpoint );
+
+	// If no record was found
+	if ( empty( $responseBody ) || empty( $responseBody[ 'data' ] ) )
+		return null;
+
+	// If more than one record was found
+	if ( $responseBody[ 'info' ][ 'count' ] > 1 ) {
+		$errorMessage = 'More than one ' . $recordType . ' found with the given criteria; ' . json_encode( $criteria ) . '.';
+		throw new \Exception( $errorMessage, 4002 );
+	}
+
+	$record = array_filter( $responseBody[ 'data' ][ 0 ] );
+	$record[ 'recordType' ] = $recordType;
+
+	return $record;
+
+}
+
+
+
+/*
+ * -----
+ * Get customer by phone number
+ * -----
+ */
+function getCustomerByPhoneNumber ( $phoneNumber, $client ) {
+
+	$customer = getRecordWhere( 'Leads', [
+		'and' => [
+			'Is_Duplicate' => 'false',
+			'Project' => [ '^=', $client ],
+			'or' => [
+				'Phone' => $phoneNumber,
+				'Alt_Mobile' => $phoneNumber
+			]
+		]
+	] );
+	if ( empty( $customer ) ) {
+		$customer = getRecordWhere( 'Contacts', [
+			'and' => [
+				'Is_Duplicate' => 'false',
+				'Project' => [ '^=', $client ],
+				'or' => [
+					'Phone' => $phoneNumber,
+					'Other_Phone' => $phoneNumber,
+					'Mobile' => $phoneNumber,
+					'Home_Phone' => $phoneNumber,
+					'Asst_Phone' => $phoneNumber
+				]
+			]
+		] );
+		if ( ! empty( $customer ) )
+			if ( $customer[ 'Stage' ] == 'Prospect' )
+				$customer[ 'isProspect' ] = true;
+	}
+
+	return $customer;
+
+}
+
