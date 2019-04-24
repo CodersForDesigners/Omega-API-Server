d9866cee204b9929edba8f3aead1df7faff6ae23
adi <adityabhat@lazaro.in>
2019-04-24 09:47:06 +0530

    Dependencies : Set up an API key refresh system for the Zoho CRM API

--- post-deploy.sh
+++ post-deploy.sh
@@ -14,3 +14,19 @@ rm __environment
 mkdir -p ../environment/${PROJECT_DIR}
 ln -s ../environment/${PROJECT_DIR} __environment
 
+# -/-/-/-/-
+# Set up all the scheduled tasks
+# -/-/-/-/-
+chmod 744 setup/zoho-refresh-api-tokens.php
+php setup/zoho-refresh-api-tokens.php
+
+if [ -f setup/tasks.crontab ]; then
+	CURRENT_WORKING_DIR=`pwd`
+	HOME=${CURRENT_WORKING_DIR}/__environment/logs
+	CRON_ENV="\n\nPATH=/bin:/usr/bin:/usr/local/bin:${CURRENT_WORKING_DIR}/setup\nHOME=${HOME}\n";
+	printf $CRON_ENV | cat - setup/tasks.crontab | tee tmp_crontab;
+	rm setup/tasks.crontab;
+	mv tmp_crontab setup/tasks.crontab;
+	cp setup/tasks.crontab ../environment/scheduled-tasks/$PROJECT_DIR.crontab
+	cat ../environment/scheduled-tasks/*.crontab | crontab -
+fi
