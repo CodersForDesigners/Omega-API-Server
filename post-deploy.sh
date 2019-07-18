f9e578e7b8ebdc2f7e9d1cad531b185ed6d2b4fa
adi <adityabhat@lazaro.in>
2019-07-18 13:59:48 +0530

    REVERT "People : Introduce a People entity/data type; entry point for all leads"
    This reverts commit cfbeba9bc9c16ea97abba22284ecd3badc42a32b.

--- post-deploy.sh
+++ post-deploy.sh
@@ -15,16 +15,6 @@ mkdir -p ../environment/${PROJECT_DIR}
 ln -s ../environment/${PROJECT_DIR} __environment
 
 # -/-/-/-/-
-# Install the third-party packages
-# -/-/-/-/-
-npm install
-
-# -/-/-/-/-
-# Reload the node processes
-# -/-/-/-/-
-pm2 reload "cupid"
-
-# -/-/-/-/-
 # Set up all the scheduled tasks
 # -/-/-/-/-
 chmod 744 setup/zoho-refresh-api-tokens.php
